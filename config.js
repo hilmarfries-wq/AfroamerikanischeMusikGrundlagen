449b037246bc09f40596381610ee57a6ba9cb026
hilmarfries-wq <hilmarfries@me.com>
2026-09-21 21:49:31 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // 2. URL und anon/public key unten eintragen.
 // 3. supabase.sql im SQL Editor ausführen.
 window.TEST_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: ""
+  SUPABASE_URL: "https://owemgypenebvynskadgi.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_iL0iy85bWf-vJmWkrjoTqg_x8BjFzG7"
 };
